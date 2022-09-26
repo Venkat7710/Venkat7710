80449276afd5135828a701226cd4e89a256e3f93
Venkat7710 <113335953+Venkat7710@users.noreply.github.com>
2022-09-26 11:32:00 +0530

    Add files via upload

---
+++ biggest number p.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+ namespace std;
+class biggest
+	cout<<"enter the two numbers:";
+	cin>>x>>y;
+};
+void biggest::display()
+{
+	if(x>y)
+	cout<<"the biggest number"<<x;
+	else
+	cout<<"the biggest number"
+{using
+	int x,y;
+	public:
+	void input(); 
+	void display();
+};
+void biggest::input()
+{<<y;
+}
+int main()
+{
+	biggest a;
+	a.input();
+	a.display();
+	return 0;
+}
