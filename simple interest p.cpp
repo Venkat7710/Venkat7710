82e8a84eba24b389511866f3811434c32700e270
Venkat7710 <113335953+Venkat7710@users.noreply.github.com>
2022-09-26 11:44:08 +0530

    Add files via upload

---
+++ simple interest p.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float p,r,t,si;
+	 cout<<"enter principle:";
+	 cin>>p;
+	 cout<<"enter rate:";
+	 cin>>r;
+	 cout<<"enter time:";
+	 cin>>t;
+	 si=p*r*t/100;
+	 cout<<"si:"<<si;
+	 return 0;
+}
